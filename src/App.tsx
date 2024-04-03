63c33d1e0207944948bb7d1bff01957830937219
brahim-belghiti <brahim.belghiti@proton.me>
2024-04-03 01:46:02 +0000

    add tenstack router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold text-orange-500">Hello world!</h1>
-    </>
-  );
+import React, { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
+
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
 }
 
-export default App;
+// Render the app
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  )
+}
